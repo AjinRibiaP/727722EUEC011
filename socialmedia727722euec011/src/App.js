2054faa2dd9237698af76d7d7c8380497794fafc
AjinRibiaP <154725167+AjinRibiaP@users.noreply.github.com>
2025-03-28 15:01:56 +0530

    Add files via upload

---
+++ socialmedia727722euec011/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopUsers from './components/TopUsers'
+import '../src/App.css'
+
+function App() {
+  return (
+    <>
+    <TopUsers />
+    </>
+  )
+}
+export default App
